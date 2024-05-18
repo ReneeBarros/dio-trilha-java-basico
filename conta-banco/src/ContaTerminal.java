ed0422f14bb4deaae96e6bd6c04178c5c8bc7f39
Renee Barros <reneebarros4@gmail.com>
2024-05-18 14:39:47 -0300

    Sistema de criação de conta de banco via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-Vindo ao Conta Banco!");
+
+        System.out.println("Vamos criar agora a sua conta, precisamos de algumas informações: ");
+
+        System.out.println("Por favor, digite o número de sua conta !");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome !");
+        String nome = scanner.next();
+
+        //scanner.next();
+
+        System.out.println("Por favor, digite o saldo !");
+        BigDecimal saldo = scanner.nextBigDecimal();
+
+        String contaCriada = "Olá ".concat(nome)+ 
+        ", Obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia) + 
+        ", conta " + conta + " e seu saldo " + saldo + " já está disponivel para saque.";
+        scanner.close();
+
+        System.out.println(contaCriada);
+
     }
 }
